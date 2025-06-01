ea746bd76b388d7e8fb62e2f368a8ff48b83f08e
Lochlan McElroy <85311595+lochlanmcelroy@users.noreply.github.com>
2025-05-31 20:49:50 -0500

    Update index.tsx

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -104,13 +104,6 @@ function App() {
         ref={canvasRef as LegacyRef<HTMLCanvasElement>}
         style={{ width: 400, height: 400, maxWidth: "100%", aspectRatio: 1 }}
       />
-
-      {/* Let's give some credit */}
-      <p>
-        Powered by <a href="https://cobe.vercel.app/">🌏 Cobe</a>,{" "}
-        <a href="https://www.npmjs.com/package/phenomenon">Phenomenon</a> and{" "}
-        <a href="https://npmjs.com/package/partyserver/">🎈 PartyServer</a>
-      </p>
     </div>
   );
 }
